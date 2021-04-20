30e744e1e9ca3a54c67b5e866eaf3c01842630ff
Abdisis Abrahim <aaaczv@umsystem.edu>
2021-04-20 10:19:46 -0500

    Added the class .h and .cpp. Added variables to the private class.

---
+++ BinaryTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Abdul Aziz Ibrahim on 4/20/21.
+//
+
+#include "BinaryTree.h"
---
+++ BinaryTree.h
@@ -0,0 +1,20 @@
+//
+// Created by Abdul Aziz Ibrahim on 4/20/21.
+//
+
+#ifndef PROJECT_5_BINARYTREE_H
+#define PROJECT_5_BINARYTREE_H
+using namespace std;
+
+class BinaryTree {
+    private:
+        struct TreeNode{
+            string value;
+            int count;
+            TreeNode* left;
+            TreeNode* right;
+        };
+};
+
+
+#endif //PROJECT_5_BINARYTREE_H
