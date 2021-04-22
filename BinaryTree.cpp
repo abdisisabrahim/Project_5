c36cb5c6cc763276a7c2aec4c68582d5ee3ccc65
jbckn <jacobbrandt3553@gmail.com>
2021-04-22 10:38:34 -0500

    Added more functions from the book

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "BinaryTree.h"
+#include <iostream>
+
+using namespace std;
 
 void BinaryTree::insertNode(int num)
 {
@@ -25,4 +28,103 @@ void BinaryTree::insert(TreeNode *&nodePtr, TreeNode *&newNode)
     insert(nodePtr->left, newNode); // Search the left branch.
     else
     insert(nodePtr->right, newNode); // Search the right branch.
+}
+
+void BinaryTree::displayInOrder(TreeNode *nodePtr) const
+{
+    if (nodePtr)
+    {
+        displayInOrder(nodePtr->left);
+        cout << nodePtr->value << endl;
+        displayInOrder(nodePtr->right);
+    }
+}
+
+void BinaryTree::displayPreOrder(TreeNode *nodePtr) const
+{
+    if (nodePtr)
+    {
+        cout << nodePtr->value << endl;
+        displayPreOrder(nodePtr->left);
+        displayPreOrder(nodePtr->right);
+    }
+}
+
+void BinaryTree::displayPostOrder(TreeNode *nodePtr) const
+{
+    if (nodePtr)
+    {
+        displayPostOrder(nodePtr->left);
+        displayPostOrder(nodePtr->right);
+        cout << nodePtr->value << endl;
+    }
+}
+
+bool BinaryTree::searchNode(int num)
+{
+    TreeNode *nodePtr = root;
+
+    while (nodePtr)
+    {
+        if (nodePtr->value == num)
+            return true;
+        else if (num < nodePtr->value)
+            nodePtr = nodePtr->left;
+        else
+            nodePtr = nodePtr->right;
+    }
+
+    return false;
+}
+
+void BinaryTree::remove(int num)
+{
+    deleteNode(num, root);
+}
+
+void BinaryTree::deleteNode(int num, TreeNode *&nodePtr)
+{
+    if (num < nodePtr->value)
+        deleteNode(num, nodePtr->left);
+    else if (num > nodePtr->value)
+        deleteNode(num, nodePtr->right);
+    else
+        makeDeletion(nodePtr);
+}
+
+void BinaryTree::makeDeletion(TreeNode *&nodePtr)
+{
+    // Define a temporary pointer to use in reattaching
+    // the left subtree.
+    TreeNode *tempNodePtr = nullptr;
+
+    if (nodePtr == nullptr)
+        cout << "Cannot delete empty node.\n";
+    else if (nodePtr->right == nullptr)
+    {
+        tempNodePtr = nodePtr;
+        nodePtr = nodePtr->left; // Reattach the left child.
+        delete tempNodePtr;
+    }
+    else if (nodePtr->left == nullptr)
+    {
+        tempNodePtr = nodePtr;
+        nodePtr = nodePtr->right; // Reattach the right child.
+        delete tempNodePtr;
+    }
+    // If the node has two children.
+    else
+    {
+        // Move one node to the right.
+        tempNodePtr = nodePtr->right;
+        // Go to the end left node.
+        while (tempNodePtr->left)
+            tempNodePtr = tempNodePtr->left;
+        // Reattach the left subtree.
+        tempNodePtr->left = nodePtr->left;
+        tempNodePtr = nodePtr;
+        // Reattach the right subtree.
+        nodePtr = nodePtr->right;
+        delete tempNodePtr;
+    }
 }
